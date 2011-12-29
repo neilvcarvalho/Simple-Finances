a3a455a6386ed5a17f7b6832581f7637b832cfe3
Neil Carvalho <neil@neil.fm>
2011-12-29 21:55:55 -0200

    Instalacao do Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleFinances::Application.config.secret_token = '9cad2d627d7e8aa79af1233736e007cf849cbf667be7f47369aee1f733b4fa29f79057adfcc59471bd376d071a11808158a688045d82c13ac000cdf5a2169317'
