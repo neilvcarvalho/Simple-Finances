21221f9ddbf26d102a24502223d2e35ec035603c
Neil Carvalho <neil@neil.fm>
2012-01-09 08:30:19 -0200

    Moves the main page to main_controller

--- app/controllers/movimentacoes_controller.rb
+++ app/controllers/movimentacoes_controller.rb
@@ -3,14 +3,6 @@
 class MovimentacoesController < ApplicationController
   before_filter :belongs_to_user, except: [:index, :create]
   def index
-    @movimentacoes = MovimentacaoDecorator.decorate(Movimentacao.where(["conta_id in (?)",current_user.contas]).order("date desc, id desc").page(params[:page]).per_page(10))
-    @categorias = CategoriaDecorator.find_all_by_user_id(current_user)
-    @contas = ContaDecorator.decorate(current_user.contas.order("owner_id, descricao"))
-    @tipos = [["gastei", "S"], ["ganhei", "E"], ["transferi", "T"]]
-    @balance = current_user.monthly_balance
-    @per_day = @balance / (Time.now.end_of_month.day - Time.now.day)
-    @last_movement = Movimentacao.where(["user_id = ?",current_user]).order("created_at DESC").first
-    @movimentacao = Movimentacao.new(categoria_id: @last_movement.try(:categoria).try(:id), conta_id: @last_movement.try(:conta).try(:id))
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SimpleFinances::Application.routes.draw do
+  get "main/index"
+
   resources :movimentacoes
 
   resources :categorias
@@ -56,7 +58,7 @@ SimpleFinances::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'movimentacoes#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
