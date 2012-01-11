59973c9666cc467c2c37deba1da809bb1ecf4447
Neilson Carvalho <neilson@maino.com.br>
2012-01-11 10:37:08 -0200

    Cleaning up some mess I did

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,14 +1,2 @@
-class Teste
-	def find_palindrome(string, init)
-		a = string.size - init
-		while a >= 2
-			n = string.size-a+1
-			while n >= 1
-				s = string.slice(n,a)
-				return s if s == s.reverse
-				n -= 1
-			end
-			a -= 1
-		end
-	end
+class ApplicationDecorator < Draper::Base
 end
