1911b3837ca0efe9e2a00878847e3bed811bb7ed
Neil Carvalho <neil@neil.fm>
2012-01-11 00:59:11 -0200

    Monthly balance per category

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -2,4 +2,8 @@ class Categoria < ActiveRecord::Base
   attr_accessible :descricao, :user_id
   belongs_to :user
   has_many :movimentacoes
+
+  def monthly_balance
+  	movimentacoes.where(["tipo = ?", "E"]).sum("quantia") - movimentacoes.where(["tipo = ?", "S"]).sum("quantia")
+  end
 end
